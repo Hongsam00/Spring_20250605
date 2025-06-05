646959ed7a43ecc48dacf66ac75af191cb9c615f
김승재 <yejiad12@gmail.com>
2025-06-05 13:24:21 +0900

    Add files via upload

---
+++ m01_01/src/main/java/com/pcwk/ehr/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Package Name : com.pcwk.ehr <br/>
+ * Class Name: Hello.java <br/> 
+ */
+package com.pcwk.ehr;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
